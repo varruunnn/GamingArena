bb8705bac7e582c53eb3f1a569840e7081bac119
varruunnn <varun.choudhary2512@gmail.com>
2024-12-21 18:02:24 +0530

    fix

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import './profile.css'
+import './Profile.css'
 
 const Profile = () => {
   const [name, setName] = useState("");
